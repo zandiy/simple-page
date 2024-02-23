cb7c81fe6ebbbed34f4cec34321820dc4cc74def
mister.xin <29502954@qq.com>
2024-02-23 15:50:46 +0800

    default file

---
+++ config/app.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    //开启[读取pathinfo];关闭[GET获取S参数]
+    'rewrite'=>true,
+];
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
+namespace misterxin;
 
 require './vendor/autoload.php';
+
+App::getInstance()->run();
